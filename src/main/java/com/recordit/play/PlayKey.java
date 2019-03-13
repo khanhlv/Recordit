bf32a301be040b39abef38f60726b051abadcc1d
khanhlv5 <khanhlv5@vpbank.com.vn>
2019-03-13 17:25:41 +0700

    update mouse listener

--- src/main/java/com/recordit/play/PlayKey.java
+++ src/main/java/com/recordit/play/PlayKey.java
@@ -5,6 +5,7 @@ import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.HashMap;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jnativehook.keyboard.NativeKeyEvent;
@@ -17,34 +18,27 @@ import org.jnativehook.keyboard.NativeKeyEvent;
 public class PlayKey {
 
     private static final File file = new File("D:/key.txt");
-
-    public static int typeKey(String character) {
-        int key = -1;
-
+    private static final HashMap<String, Integer> hasKey = new HashMap<>();
+    static {
         for (int i = 0 ; i < 16*16*16*16; i++) {
-            if (StringUtils.equals(KeyEvent.getKeyText(i), character)) {
-                key = i;
-                break;
-            }
+            hasKey.put(KeyEvent.getKeyText(i), i);
         }
-
-        return key;
     }
 
     public static void play() throws Exception {
         BufferedReader in = new BufferedReader(new FileReader(file));
         Robot robot = new Robot();
-
+        Thread.sleep(4000);
         String input = null;
         while((input = in.readLine()) != null) {
             String[] splitKey = input.split("=");
 
             if (StringUtils.equals("KEY_PRESSED", splitKey[0])) {
-                robot.keyPress(typeKey(splitKey[1]));
+                robot.keyPress(hasKey.get(splitKey[1]));
             }
 
             if (StringUtils.equals("KEY_RELEASED", splitKey[0])) {
-                robot.keyPress(typeKey(splitKey[1]));
+                robot.keyRelease(hasKey.get(splitKey[1]));
             }
 
             Thread.sleep(20);
