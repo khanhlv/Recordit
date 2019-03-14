97e560ba458f9e2dd3635c466e4bfba311f9be0d
khanhlv5 <khanhlv5@vpbank.com.vn>
2019-03-14 15:53:06 +0700

    update mouse listener

--- src/main/java/com/recordit/Application.java
+++ src/main/java/com/recordit/Application.java
@@ -1,6 +1,7 @@
 package com.recordit;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import com.recordit.listener.RecorditListener;
 import com.recordit.play.RecorditPlay;
@@ -10,12 +11,19 @@ public class Application {
         System.out.println("Thong tin tham so");
         System.out.println("1 - Ghi lai thao tac chuot va ban phim");
         System.out.println("2 - Phat lai thao tac chuot va ban phim");
-        if (args != null) {
+        System.out.println("2 2 - Phat lai thao tac chuot va ban phim voi so lan");
+        if (args != null && args.length > 0) {
             if (StringUtils.equals("1", args[0])) {
                 RecorditListener.execute();
             }
             if (StringUtils.equals("2", args[0])) {
-                RecorditPlay.execute();
+                if (StringUtils.isNotBlank(args[1])) {
+                    for (int i = 0; i <= NumberUtils.toInt(args[1]); i++) {
+                        RecorditPlay.execute();
+                    }
+                } else {
+                    RecorditPlay.execute();
+                }
             }
         } else {
             System.out.println("Thieu tham so");
