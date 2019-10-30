24277f3190b1bd28d89f99aa9acc0198d4861dac
khanhlv5 <khanhlv5@vpbank.com.vn>
2019-10-30 15:40:21 +0700

    update

--- src/main/java/com/recordit/compare/ImageComparison.java
+++ src/main/java/com/recordit/compare/ImageComparison.java
@@ -2,8 +2,26 @@ package com.recordit.compare;
 
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ImageComparison {
+    private static final Logger logger = LoggerFactory.getLogger(ImageComparison.class);
+
+    public static boolean findImage(String inputFile) {
+        try {
+            Screen screen = new Screen();
+
+            Pattern pattern = new Pattern(inputFile);
+
+            screen.find(pattern);
+
+            return true;
+        } catch (Exception ex) {
+            logger.error("ImageComparison", ex);
+            return false;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         Screen screen = new Screen();
@@ -11,7 +29,6 @@ public class ImageComparison {
         Pattern openButton = new Pattern( "D:\\Recordit\\data\\check.png");
 
         screen.find(openButton);
-
 
     }
 }
